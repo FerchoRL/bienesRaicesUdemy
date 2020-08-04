249f72d345e0ba9b56bd213a120f3d7f75e38fd4
Mr Garrix RL <rlfernando7@gmail.com>
2020-08-03 21:27:52 -0500

    Completados los archivos de anuncios, anuncio y nosotros.

--- anuncios.php
+++ anuncios.php
@@ -2,12 +2,197 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Anuncios</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta >
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/fontawesome-free-5.14.0-web/css/all.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+	<title>Bienes Raices</title>
 </head>
+<!--=====================================
+=            Section comment            =
+
+comm-html-section + tab = insert comment section
+======================================-->
+
+
+
+<!--====  End of Section comment  ====-->
+
 <body>
 
-	<!-- Include navbar file using php -->
-	<?php include 'navbarHeader.php' ?>
+	<header>
+		<!-- Include navbar file using php -->
+		<?php include 'navbarHeader.php' ?>
+	</header>
+
+	<!-- Main -->	
+	<main class="contenedor seccion">
+		<h2 class="text-center font-weight-bold">Casas y Depas en venta</h2>
+
+		<div class="contenedor-anuncios">
+			<div class="anuncio">
+				<img src="img/anuncio1.jpg" alt="Anuncio casa en el lago">
+				<div class="contenido-anuncio">
+					<h3>Casa de Lujo en el lago</h3>
+					<p>Casa en el lago con excelente vista, acabados de lujo a excelente precio</p>
+					<ul class="iconos-anuncio">
+						<li>
+							<i class="fas fa-toilet fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+						<li>
+							<i class="fas fa-car fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+						<li>
+							<i class="fas fa-bed fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+					</ul>
+					<p class="precio">$3,000,0000</p>
+					<a href="anuncio.php" class="btn btn-info" role="button" aria-pressed="true">Ver propiedad<i class="far fa-file-alt"></i></a>
+				</div>
+			</div>
+			<!-- Fin anuncio -->
+
+			<div class="anuncio">
+				<img src="img/anuncio2.jpg" alt="Anuncio casa de lujo">
+				<div class="contenido-anuncio">
+					<h3>Casa Terminados de Lujo</h3>
+					<p>Casa con diseño moderno, asi como tecnologia inteligente y amueblada</p>
+					<ul class="iconos-anuncio">
+						<li>
+							<i class="fas fa-toilet fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+						<li>
+							<i class="fas fa-car fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+						<li>
+							<i class="fas fa-bed fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+					</ul>
+					<p class="precio">$2,000,0000</p>
+					<a href="anuncio.php" class="btn btn-info" role="button" aria-pressed="true">Ver propiedad<i class="far fa-file-alt"></i></a>
+				</div>
+			</div>
+			<!-- Fin anuncio -->
+
+			<div class="anuncio">
+				<img src="img/anuncio3.jpg" alt="Anuncio casa con alberca">
+				<div class="contenido-anuncio">
+					<h3>Casa con Alberca</h3>
+					<p>Casa con alberca y acabados de lujo en la ciudad, excelente oportunidad</p>
+					<ul class="iconos-anuncio">
+						<li>
+							<i class="fas fa-toilet fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+						<li>
+							<i class="fas fa-car fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+						<li>
+							<i class="fas fa-bed fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+					</ul>
+					<p class="precio">$3,000,0000</p>
+					<a href="anuncio.php" class="btn btn-info" role="button" aria-pressed="true">Ver propiedad<i class="far fa-file-alt"></i></a>
+				</div>
+			</div>
+			<!-- Fin anuncio -->
+
+			<div class="anuncio">
+				<img src="img/anuncio4.jpg" alt="Anuncio casa de lujo">
+				<div class="contenido-anuncio">
+					<h3>Casa fuera de la ciudad</h3>
+					<p>Casa con alberca y acabados de lujo fuera de la ciudad, excelente oportunidad</p>
+					<ul class="iconos-anuncio">
+						<li>
+							<i class="fas fa-toilet fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+						<li>
+							<i class="fas fa-car fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+						<li>
+							<i class="fas fa-bed fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+					</ul>
+					<p class="precio">$3,000,0000</p>
+					<a href="anuncio.php" class="btn btn-info" role="button" aria-pressed="true">Ver propiedad<i class="far fa-file-alt"></i></a>
+				</div>
+			</div>
+			<!-- Fin anuncio -->
+
+			<div class="anuncio">
+				<img src="img/anuncio5.jpg" alt="Anuncio casa frente al bosque">
+				<div class="contenido-anuncio">
+					<h3>Casa con Alberca</h3>
+					<p>Casa con alberca y acabados de lujo frente al bosque, excelente oportunidad</p>
+					<ul class="iconos-anuncio">
+						<li>
+							<i class="fas fa-toilet fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+						<li>
+							<i class="fas fa-car fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+						<li>
+							<i class="fas fa-bed fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+					</ul>
+					<p class="precio">$3,000,0000</p>
+					<a href="anuncio.php" class="btn btn-info" role="button" aria-pressed="true">Ver propiedad<i class="far fa-file-alt"></i></a>
+				</div>
+			</div>
+			<!-- Fin anuncio -->
+
+			<div class="anuncio">
+				<img src="img/anuncio6.jpg" alt="Anuncio casa con alberca">
+				<div class="contenido-anuncio">
+					<h3>Casa con Alberca</h3>
+					<p>Casa con alberca y acabados de lujo en la ciudad, excelente oportunidad</p>
+					<ul class="iconos-anuncio">
+						<li>
+							<i class="fas fa-toilet fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+						<li>
+							<i class="fas fa-car fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+						<li>
+							<i class="fas fa-bed fa-3x" alt="icono wc"></i>
+							<p>3</p>
+						</li>
+					</ul>
+					<p class="precio">$3,000,0000</p>
+					<a href="anuncio.php" class="btn btn-info" role="button" aria-pressed="true">Ver propiedad<i class="far fa-file-alt"></i></a>
+				</div>
+			</div>
+			<!-- Fin anuncio -->
+			
+		</div>
+		<!-- Fin contenedor anuncios -->
+
+	</main>
+	<!-- Footer -->
+	<?php include 'navbarFooter.php'; ?>
+
+	<!-- If you’re using our compiled JavaScript, don’t forget to include CDN versions of jQuery and Popper.js before it  -->
+
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="js/bootstrap.js"></script>
 	
 </body>
 </html>
--- nosotros.php
+++ nosotros.php
@@ -2,12 +2,81 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Nosotros</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta >
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/fontawesome-free-5.14.0-web/css/all.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+	<title>Bienes Raices</title>
 </head>
+<!--=====================================
+=            Section comment            =
+
+comm-html-section + tab = insert comment section
+======================================-->
+
+
+
+<!--====  End of Section comment  ====-->
+
 <body>
 
-	<!-- Include navbar file using php -->
-	<?php include 'navbarHeader.php' ?>
+	<header>
+		<!-- Include navbar file using php -->
+		<?php include 'navbarHeader.php' ?>
+	</header>
+
+	<!-- Main -->
+	
+	<main class="contenedor seccion">
+		<h1 class="text-center font-weight-bold">Conoce Sobre Nosotros</h1>
+		<div class="contenido-nosotros">
+			<div>
+				<img src="img/nosotros.jpg" alt="Imagen sobre nosotros">
+			</div>
+			<div class="texto-nosotros">
+				<blockquote>25 Años de Experiencia</blockquote>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis dicta qui aliquid, et temporibus sunt neque dolorum voluptatem ipsum adipisci, eveniet nam animi fugit, autem modi cumque debitis libero nesciunt?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius molestias minus harum at quod doloribus sed ullam inventore possimus tenetur ipsa laboriosam saepe, alias expedita, modi nostrum nemo! Exercitationem, deleniti.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias laudantium qui tempore provident amet commodi, in, aspernatur veniam id possimus, sint numquam totam necessitatibus voluptatum tempora. Sed architecto, nobis quisquam!
+				</p>
+
+			</div>
+		</div>
+	</main>
+
+	<!-- Mas sobre nosotros section -->
+	
+	<section class="contenedor seccion">
+		<h2 class="text-center font-weight-bold">Mas sobre nosotros</h2>
+		<div class="iconos-nosotros">
+			<div class="icono-hijo">
+				<img src="img/icono1.svg" alt="Icono seguridad">
+				<h3>Seguridad</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non inventore laborum maiores, labore ea officia fugiat atque accusamus porro sint, laboriosam explicabo quibusdam minima earum distinctio sed ducimus sapiente? Sint!</p>
+			</div>
+			
+			<div class="icono-hijo">
+				<img src="img/icono2.svg" alt="Icono mejor precio">
+				<h3>El mejor precio</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis hic, dolores unde perferendis ipsam doloremque ab eaque, a fuga. Ut laboriosam assumenda eum voluptatum quo ipsum. Possimus, dignissimos dicta tempora!</p>
+			</div>
+			
+			<div class="icono-hijo">
+				<img src="img/icono3.svg" alt="Icono a tiempo">
+				<h3>A tiempo</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque quo animi expedita aspernatur excepturi eos distinctio qui, incidunt omnis libero molestias minus, fugiat vitae illo maiores. Ab eveniet, accusamus vitae.</p>
+			</div>
+		</div>
+	</section>
+
+	<!-- Footer -->
+	<?php include 'navbarFooter.php'; ?>
+
+	<!-- If you’re using our compiled JavaScript, don’t forget to include CDN versions of jQuery and Popper.js before it  -->
+
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="js/bootstrap.js"></script>
 	
 </body>
 </html>
